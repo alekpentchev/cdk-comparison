f1ea0e54c1d1cc0785ff6daea4872700d7720bf8
Aleksander Pentchev <alek.pentchev@gmail.com>
2024-01-22 15:55:42 +0100

    comments

--- cdk/aws_cdk_api_stack.py
+++ cdk/aws_cdk_api_stack.py
@@ -9,13 +9,11 @@ from .data_stack import DataStack
 
 class AwsCdkApiStack(Stack):
     
-    # AWS article on 3 tier web app
-    # https://docs.aws.amazon.com/whitepapers/latest/serverless-multi-tier-architectures-api-gateway-lambda/presentation-tier.html
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-         # 3 tier web app divided into 4 stacks
+        # web app divided into 3 stacks
         # 1. Network stack - public and private subnets
         self.network_stack = NetworkStack(self, 'network')
         # 2. Data stack - DynamoDB table
